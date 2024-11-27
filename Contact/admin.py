d356c7f120f913dacafbd3eaaee1370800820b6f
Willian Lopes <willian1999lopes@gmail.com>
2024-11-26 21:50:08 -0300

    inserindo imagem no contato e config url para imagem no settings

--- Contact/admin.py
+++ Contact/admin.py
@@ -3,4 +3,33 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = (
+        'first_name',
+        'last_name',
+        'phone',
+    )
+
+    ordering = (
+        'first_name',
+    )
+
+    list_filter = (
+        # 'created_date',
+    )
+
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+
+    list_display_links = 'first_name',
+
+    list_per_page = 10
+
+    list_max_show_all = 300
+
+    # list_editable = (
+    #     'first_name',
+    #     'last_name',
+    # )
